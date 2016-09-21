629e27c9c0a4d31f6aa91f591d734213ec8b3aea
Charles Bezerra <charles-bezerra>
2016-09-21 08:55:44 -0300

    Alterações finais

--- Projeto_ControladorTemp.ino
+++ Projeto_ControladorTemp.ino
@@ -1,55 +1,112 @@
-#include<LiquidCrystal.h>
-LiquidCrystal lcd(12,11,5,4,6,2);
-long int pino = 2;
-long int pinoMotor = 3;
+#include<LiquidCrystal.h>                //estou importando a biblioteca do lcd
+LiquidCrystal lcd(12,11,5,4,3,2);        //aqui estou definindo os pinos do lcd
+long int pinoTransistor = 6;         
 
+/*Função para formatar os valeros analogicos dados pelo sensor de temperatura, 
+retornando o valor da temperatura ambiente.*/
 int convercaoTemp(){
       long int pinoSensor = 0;
       int temperatura;
       temperatura = map(analogRead(pinoSensor),0,102,0,50);
       return temperatura;
 }
-int convercaoValorDesejado(){
+
+
+
+/* Função para formatar os valeros analogicos dados pelo potenciometro, 
+retornando o valor da temperatura desejada.*/
+int convercaoValorDesejado(){  
       int valorDesejado;
       long int pinoleituraPotenciometro = 5;
-      valorDesejado = map(analogRead(pinoleituraPotenciometro), 1080,0,0,50);
+      valorDesejado = map(analogRead(pinoleituraPotenciometro), 1080,0,15,36);
       return valorDesejado;
 }
+
+
+
+/* Função que reberá como parametro dois argumentos x e y para serem 
+ *  impressos no LCD 16x2, 
+  x será a temperatura abiente e sera impresso na parte superior do LCD
+  y será a temperatura desejada e sera impresso na parte inferior do LCD
+ */ 
+int imprimir_valores_lcd(int x,int y){
+      lcd.clear();               //Limpa a tela  do lcd
+      lcd.print("TempAmb: ");    // imprimi o conteúdo entre aspas
+      lcd.setCursor(0,0);        // imprimi o conteúdo anterior na coluna 0 e na linha 0
+      lcd.print(x);              // imprimindo o valor de x,por  parametro a temperatura ambiente 
+      lcd.setCursor(7,0);        // difino a coluna 7 na linha 0
+      lcd.print("TempDesej: ");
+      lcd.setCursor(0,2);     
+      lcd.print(y);              //imprimi o valor de y, por  parametro a temperatura desejada
+      lcd.setCursor(1,8);
+}
+
+
+
+int serialImprimir(int x, int y){
+      Serial.print("Tenperatura: ");
+      Serial.println(x);
+      Serial.print("Temperatura desejada: ");
+      Serial.println(y);
+}
+
+
+
+/*Essa Função foi criada com intuito de abestrair o valor 
+ * analogico dado pelo pino do arduino que esta ligado 
+  a base do transistor, regulando a quantidade 
+  de giros do motor. A função pega a diferença de temperatura 
+  da temperatura ambiente e a 
+  temperatura desejada, para ser dado o valor 
+  analogico correspondente esse valor multiplicado 
+  por 7,5 que é a constante tirado de uma 
+  equação do primeiro grau simples, onde 
+  pegamos a menor temperatura que podemos chegar 
+  que 16 graus C*, com diferença da maior temp fornecida que 
+  podemos obserar pelo o sensor que é 50 graus C
+  tem multiplicado por x dando igual ao valor anoligico 5V que é 255.*/
+int Vanalog_Diferenca_Temp(int x, int y){ 
+      float j =  x - y;
+      return j * 7.5;    
+}
+
+
+
+
+
 void setup(){
       Serial.begin(9600);
-      lcd.begin(16,2);
-      pinMode(pino, OUTPUT);
-      digitalWrite(pino, HIGH); //Pino do potenciomentro, onde a há a fonte de tenção
-      pinMode(pinoMotor, OUTPUT);
+      lcd.begin(16,2); // aqui estou especificando a quantidade de lacunas e linhas do lcd
+      pinMode(pinoTransistor, OUTPUT);
       
 }
 
 
 
 void loop(){ 
-  if( convercaoTemp() > 60){
-      Serial.println("Temperatura inválida");  
-  } 
   
-  else{
-      lcd.print("TempAmb: ", convercaoTemp()); 
-      lcd.setCursor(0,1);
-      lcd.print("TempDesej: ", convercaoValorDesejado()); 
-      Serial.print("Tenperatura: ");
-      Serial.println(convercaoTemp());
-      Serial.print("Temperatura desejada: ");
-      Serial.println(convercaoValorDesejado());
+  //Este if limita o codigo de receber valores correspondentes as temperaturas acima de 50 
+  if( convercaoTemp() > 50){
+      lcd.clear();
+      lcd.print("Temp Perigosa!")
+      Serial.println("Temperatura Perigosa!");  }
       
-      if(convercaoValorDesejado() < convercaoTemp()){
-        
-               // a encrementação tem que ser aqui
-                digitalWrite(pinoMotor,HIGH);
-               
-        }
+  else{ 
+      /*aqui estou chamando a função para printar os conteudos para o lcd, 
+      passando por parametros valores das funçoes que retornam a 
+      temperatura ambiente e temperatura desejada */
+      imprimir_valores_lcd(convercaoTemp(),convercaoValorDesejado()); 
+       
+      /*aqui estou chamando a função para printar os conteudos por comunicação serial, 
+      passando por parametros valores das funçoes que retornam a t
+      emperatura ambiente e temperatura desejada */
+      serialImprimir(convercaoTemp(), convercaoValorDesejado());  
+
+      /*Nesse if verificamos se a temperatura deseja é menor que a temperatura ambiente, se caso for e pino 
+       do transistor é ativado na intensidade desejada, se caso for menor o pino do transistor é desligado */
+      if(convercaoValorDesejado() < convercaoTemp()){              
+            analogWrite( pinoTransistor ,Vanalog_Diferenca_Temp( convercaoTemp(),convercaoValorDesejado() ) );  }
       else {               
-                digitalWrite(pinoMotor, LOW);
+            digitalWrite(pinoTransistor, LOW);  }
       }
-      delay(900);
-  }
- 
 }
